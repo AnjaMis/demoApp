66670729dd93f96d10de42fcda2de4f7e2804fdf
AnjaMis <bgp916@myy.haaga-helia.fi>
2022-02-27 03:43:44 +0200

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native'
+import React, { useState } from 'react'
 
 export default function App() {
+  const [text, setText] = useState('')
+
+  const buttonPressed = () => {
+    Alert.alert('Hello', 'You typed: ' + text)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{ width: 200, borderColor: 'grey', borderWidth: 1 }}
+        value={text}
+        onChangeText={(text) => setText(text)}
+      />
+
+      <Button onPress={buttonPressed} title="press me"></Button>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +29,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
